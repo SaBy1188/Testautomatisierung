3664d92c8ebe19dfe3203b526e12795f3515e867
Sabrin <sabrin.byaah@brightstraining.com>
2023-03-27 15:35:40 +0200

    Initial commit

---
+++ src/main/java/de/awacademy/linkz/user/UserDTO.java
@@ -0,0 +1,12 @@
+package de.awacademy.linkz.user;
+
+public class UserDTO {
+    String username;
+    String password;
+
+    public void setUsername(String username) {
+    }
+
+    public void setEncodedPassword(String password) {
+    }
+}
